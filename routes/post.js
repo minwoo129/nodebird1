28c5b2149e3a10b8911c7ce0d70ab0e057bcf1ff
minwoo129 <key0129mw@naver.com>
2022-09-04 21:20:48 +0900

    파일 업로드 연결

---
+++ routes/post.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const { Post, Hashtag } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+try {
+    fs.readdirSync('uploads');
+} catch (err) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        }
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 }
+});
+
+router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
+    console.log(req.file);
+    res.json({ url: `/img/${req.file.filename}` });
+});
+
+const upload2 = multer();
+router.post('/', isLoggedIn, upload2.none(), async(req, res, next) => {
+    try {
+        const {content, url} = req.body;
+        const {id} = req.user;
+        const post = await Post.create({
+            content,
+            img: url,
+            UserId: id
+        });
+        const hashtags = content.match(/#[^\s#]+/g);
+        if(hashtags) {
+            const result = await Promise.all(
+                hashtags.map(tag => {
+                    return Hashtag.findOrCreate({
+                        where: { title: tag.slice(1).toLowerCase() }
+                    })
+                })
+            );
+            await post.addHashTags(result.map(r => r[0]));
+        }
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+module.exports = router;

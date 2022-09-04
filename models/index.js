1b1c48470602b3fedc4f8b1be421b8e4aed16fcc
minwoo129 <key0129mw@naver.com>
2022-09-04 22:33:47 +0900

    mysql 연결 시도

--- models/index.js
+++ models/index.js
@@ -4,9 +4,18 @@ const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
+const mysql = require('mysql2');
 
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const cn = mysql.createConnection({
+    host: config.host,
+    user: config.username,
+    password: config.password,
+    database: config.database,
+    port: '3306'
+});
+cn.connect();
 
 db.sequelize = sequelize;
 db.User = User;

4c1f1f801621b8a39b64e2cf4be269012558d0ab
minwoo129 <key0129mw@naver.com>
2022-09-04 20:57:56 +0900

    passport로 로그인 구현

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
 const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
-app.set('port', process.env.PORT || 8001);
+passportConfig();
+app.set('port', process.env.PORT || 8002);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
@@ -39,8 +43,11 @@ app.use(session({
         secure: false
     }
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
